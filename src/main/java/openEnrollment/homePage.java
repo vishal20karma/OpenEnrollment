00965a9c2c5323bc8397a88b7bc2da6c43c068a7
Vishal V <vishal.vishwakarma20@gmail.com>
2025-02-13 11:04:13 +0530

    initial commit

---
+++ src/main/java/openEnrollment/homePage.java
@@ -0,0 +1,12 @@
+package openEnrollment;
+
+public class homePage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.out.println("Hello There !!!");
+	
+	}
+
+}
